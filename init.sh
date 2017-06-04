ce0385949d7b99e29f653d17102622b546b90e7f
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2017-06-04 14:41:01 +0530

    Add dependencies to the script

--- init.sh
+++ init.sh
@@ -4,13 +4,35 @@
 # Synchronizes development and production instances of a Project.
 #
 # Environment Variables:
-# $FTP_USER
-# $FTP_PASSWORD
-# $FTP_URL
-#
+# $FTP_USER - FTP Username
+# $FTP_PASSWORD - FTP Password
+# $FTP_URL - URL with the directory location
+#            (eg. sftp://example.com/var/www/)
 
-# before_deploy
-# sudo apt-get install git-ftp
+export LOC=$(pwd)
+
+cd ..
+wget https://www.libssh2.org/download/libssh2-1.8.0.tar.gz
+tar -zxvf libssh2-1.8.0.tar.gz
+cd libssh2-1.8.0/
+./configure
+make
+sudo make install
+
+cd ..
+wget https://curl.haxx.se/download/curl-7.54.0.tar.gz
+tar -zxvf curl-7.54.0.tar.gz
+cd curl-7.54.0
+./configure --with-libssh2=/usr/local
+make
+sudo make install
+
+cd /usr/lib/x86_64-linux-gnu/
+sudo mv /usr/lib/x86_64-linux-gnu/libcurl.so.4 libcurl.so.4.bak
+sudo cp /usr/local/lib/libcurl.so.4 .
+sudo cp /usr/local/lib/libcurl.so.4.4.0 .
+
+cd $LOC
 
 git config git-ftp.url $FTP_URL
 git config git-ftp.user $FTP_USER
